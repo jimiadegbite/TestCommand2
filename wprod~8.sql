84632df39b18fc3fbcef6b851d9221e6134fa24a
Jimi Adegbite <jimiadegbite@gmail.com>
2022-03-13 20:35:27 -0400

    Commit test

---
+++ wprod~8.sql
@@ -0,0 +1,3 @@
+
+select "Alias_Claims_Ref_CMC_PRPR_PROV"."PRPR_ID" "Referring_Provider_ID", "Alias_Claims_Ref_CMC_PRPR_PROV"."PRPR_NAME" "Referencing_Provider_Name"
+ from "FACETS"."CMC_PRPR_PROV" "Alias_Claims_Ref_CMC_PRPR_PROV"
